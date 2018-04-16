a37b174eb265c224d80d5f56a4926840993a3565
Adrian Darian <adarian@ucmerced.edu>
2018-04-16 03:14:16 -0700

    reordered the files to better structure the content

--- src/index.js
+++ src/index.js
@@ -1,90 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import PlayerAPI from './PlayerAPI';
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
 
-const Header = () => {
-  <header>
-    <nav>
-      <ul> 
-        <li><Link to="/">Home</Link></li>
-}
-
-const FullRoster = () => {
-  <div>
-    <ul>
-    {
-      PlayerAPI.all().map(p => (
-        <li key={p.number}>
-          <Link to={`/roster/${p.number}`}>{p.name}</Link>
-        </li>
-      ))
-    }
-    </ul>
-  </div>
-}
-
-const Schedule = () => {
-  <div>
-    <ul>
-      <li>6/5 @ Evergreens</li>
-      <li>6/8 @ Kickers</li>
-      <li>6/14 @ United</li>
-    </ul>
-  </div>
-}
-
-const Home = () => {
-  <div>
-    <h1>Welcome to the Tornadoes Website!</h1>
-  </div>
-}
-
-const Player = (props) => {
-  const player = PlayerAPI.get(
-    parseInt(props.match.params.number, 10)
-  )
-  if (!player) {
-    return <div> SOrry, but the player was not found</div>
-  }
-
-  return (
-    <div>
-      <h1>{player.name} (#{player.number})</h1>
-      <h2>{player.position}</h2>
-    </div>
-  )
-}
-
-const Roster = () => {
-  <div>
-    <h2>This is a roster page!</h2>
-    <Switch>
-      <Route exact path="/roster" component={FullRoster} />
-      <Route path="/roster/:number" component={Players} />
-    </Switch>
-  </div>;
-};
-
-const Main = () => {
-  <main>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/roster" component={Roster} />
-      <Route path="/schedule" component={Schedule} />
-    </Switch>
-  </main>;
-};
-
-//this component will be rendered by our <BrowserRouter>
-const App = () => {
-  <div>
-    <Header />
-    <Main />
-  </div>;
-};
-
-ReactDOM.render(
+render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
